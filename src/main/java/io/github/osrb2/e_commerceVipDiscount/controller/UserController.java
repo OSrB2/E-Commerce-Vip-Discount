6d546cc82042c47e936037bf80aa1df255f20b40
Pedro Oliveira <pedro_oliva@outlook.com.br>
2026-03-13 20:38:24 -0300

    feat: add user delete endpoint

--- src/main/java/io/github/osrb2/e_commerceVipDiscount/controller/UserController.java
+++ src/main/java/io/github/osrb2/e_commerceVipDiscount/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,11 @@ public class UserController {
   public ResponseEntity<UserModel> updateUser(@PathVariable Long id, @RequestBody @Valid UserModel user) {
     user.setId(id);
     return ResponseEntity.ok(service.updateUserById(user));  
-    
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<UserModel> deleteUser(@PathVariable Long id) {
+    service.deleteUserById(id);
+    return ResponseEntity.noContent().build();
   }
 }
