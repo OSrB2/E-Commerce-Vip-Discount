399ae03694ede478ccd22a5d52f5ce52c1c4d470
Pedro Oliveira <pedro_oliva@outlook.com.br>
2026-03-14 19:46:33 -0300

    refact: discount service, discount strategy, vip discount

--- src/main/java/io/github/osrb2/e_commerceVipDiscount/service/DiscountService.java
+++ src/main/java/io/github/osrb2/e_commerceVipDiscount/service/DiscountService.java
@@ -1,23 +1,23 @@
 package io.github.osrb2.e_commerceVipDiscount.service;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import io.github.osrb2.e_commerceVipDiscount.model.UserModel;
 import io.github.osrb2.e_commerceVipDiscount.service.strategy.DiscountStrategy;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class DiscountService {
   
   private final Map<String, DiscountStrategy> strategies;
 
-  public DiscountService(Map<String, DiscountStrategy> strategies) {
-    this.strategies = strategies;
+  public BigDecimal applyDiscount(UserModel user, BigDecimal total) {
+    DiscountStrategy strategy = strategies.get(user.getRole());
+    return strategy.applyDiscount(total);
   }
-
-  public double applyDiscount(UserModel user, double amount) {
-    DiscountStrategy strategy = strategies.get(user.getRole().name());
-    return strategy.applyDiscount(amount);
-  }
+ 
 }
--- src/main/java/io/github/osrb2/e_commerceVipDiscount/service/strategy/DiscountStrategy.java
+++ src/main/java/io/github/osrb2/e_commerceVipDiscount/service/strategy/DiscountStrategy.java
@@ -1,5 +1,10 @@
 package io.github.osrb2.e_commerceVipDiscount.service.strategy;
 
+import java.math.BigDecimal;
+
+import io.github.osrb2.e_commerceVipDiscount.model.enums.UserRole;
+
 public interface DiscountStrategy {
-  double applyDiscount(double amount);
+  UserRole getRole();
+  BigDecimal applyDiscount(BigDecimal amount);
 }
