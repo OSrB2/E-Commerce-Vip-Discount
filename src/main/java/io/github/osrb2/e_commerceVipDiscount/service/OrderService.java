966441c892a0b704eb30de55a94472a86faa3f72
Pedro Oliveira <pedro_oliva@outlook.com.br>
2026-04-17 15:57:29 -0300

    remove discount service, refac order service

--- src/main/java/io/github/osrb2/e_commerceVipDiscount/service/OrderService.java
+++ src/main/java/io/github/osrb2/e_commerceVipDiscount/service/OrderService.java
@@ -6,23 +6,39 @@ import org.springframework.stereotype.Service;
 
 import io.github.osrb2.e_commerceVipDiscount.model.OrderModel;
 import io.github.osrb2.e_commerceVipDiscount.model.UserModel;
+import io.github.osrb2.e_commerceVipDiscount.model.enums.DiscountType;
+import io.github.osrb2.e_commerceVipDiscount.model.enums.OrderStatus;
+import io.github.osrb2.e_commerceVipDiscount.repository.OrderRepopsitory;
+import io.github.osrb2.e_commerceVipDiscount.service.strategy.DiscountStrategyContext;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class OrderService {
-  private final DiscountService discountService;
 
-  public OrderModel createOrder(UserModel user, BigDecimal totalGross) {
+  private final OrderRepopsitory orderRepopsitory;
+  private final DiscountStrategyContext discountContext;
 
-    BigDecimal totalNet = discountService.applyDiscount(user, totalGross);
+  public OrderModel createOrder(OrderModel order, UserModel user) {
 
-    OrderModel order = new OrderModel();
+    BigDecimal totalGross = calculateGross(order);
+    BigDecimal totalNet =  discountContext.applyDiscount(user.getRole(), totalGross);
+    BigDecimal discountAmount = totalGross.subtract(totalNet);
+
     order.setUser(user);
     order.setTotalGross(totalGross);
     order.setTotalNet(totalNet);
-    order.setDiscountAmount(totalGross.subtract(totalNet));
+    order.setDiscountAmount(discountAmount);
+    order.setDiscountType(DiscountType.valueOf(user.getRole().name()));
+    order.setStatus(OrderStatus.PENDING);
 
-    return order;
+    return orderRepopsitory.save(order);
+  }
+
+  private BigDecimal calculateGross(OrderModel order) {
+    return order.getItems().stream()
+    .map(item -> item.getPriceAtPurchase()
+      .multiply(BigDecimal.valueOf(item.getQuantity())))
+    .reduce(BigDecimal.ZERO, BigDecimal::add);
   }
 }
