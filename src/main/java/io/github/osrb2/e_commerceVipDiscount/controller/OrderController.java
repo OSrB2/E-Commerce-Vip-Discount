3d955322ace29883d38d6ba72df5197cfbe49d5b
Pedro Oliveira <pedro_oliva@outlook.com.br>
2026-04-17 16:32:03 -0300

    fix: getmapping

--- src/main/java/io/github/osrb2/e_commerceVipDiscount/controller/OrderController.java
+++ src/main/java/io/github/osrb2/e_commerceVipDiscount/controller/OrderController.java
@@ -3,6 +3,7 @@ package io.github.osrb2.e_commerceVipDiscount.controller;
 import io.github.osrb2.e_commerceVipDiscount.repository.OrderRepopsitory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,6 +37,7 @@ public class OrderController {
     return ResponseEntity.status(HttpStatus.CREATED).body(created);
   }
 
+  @GetMapping("/{id}")
   public ResponseEntity<OrderModel> findById(@PathVariable Long id) {
     return orderRepository.findById(id)
     .map(ResponseEntity::ok)
